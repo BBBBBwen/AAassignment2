be1ae9c533edf7fc8cbb2b802b057a2a5513073b
bowen zhang <bowen0225@users.noreply.github.com>
2019-10-17 00:38:53 +1100

    version 0.2
    hunt&kill normal maze complete
    tested
    partially refactored

--- mazeGenerator/HuntAndKillGenerator.java
+++ mazeGenerator/HuntAndKillGenerator.java
@@ -1,13 +1,15 @@
 package mazeGenerator;
 
-import maze.Maze;
+import maze.*;
 import java.util.Random;
+import java.util.*;
+
 public class HuntAndKillGenerator implements MazeGenerator {
 	private Random rand = new Random();
 	private int randC;
 	private int randR;
-	private final Integer[] array = new Integer[] { 0, 2, 3, 5 };
-	private final List<Integer> direction = Arrays.asList(array);
+	private Integer[] array = new Integer[] { 0, 2, 3, 5 };
+	private List<Integer> direction = Arrays.asList(array);
 
 	@Override
 	public void generateMaze(Maze maze) {
@@ -19,43 +21,51 @@ public class HuntAndKillGenerator implements MazeGenerator {
 
 	private void normalMazeGenerator(Maze maze) {
 		boolean[][] visited = new boolean[maze.sizeR][maze.sizeC];
-		r = randR;
-		c = randC;//random starting cell required
-		boolean hunt = true;
+		int         r = randR;
+		int         c = randC;//random starting cell required
+		boolean     hunt = true;
 
 		while(hunt) {
 			boolean kill = true;
 			do {
-				int i = 0;
 				Collections.shuffle(direction);
-				visited[r][c] = true;
-				while(visited[r + Maze.deltaR[direction[i]]][c + Maze.deltaC[direction[i]]]) {
-					++i;
-				}
-				carve(maze.map[r][c], direction[i]);
-				r += Maze.deltaR[direction[i]];
-				c += Maze.deltaC[direction[i]];
 				kill = false;
-
-				for(int j = 0; j != 4; ++j) {
-					if(visited[r + Maze.deltaR[direction[i]]][c + Maze.deltaC[direction[i]]] == false) {
-						kill = true;
+				visited[r][c] = true;
+				int dir = 0;
+				for(int k = 0; k < 4; ++k) {
+					int row = r + Maze.deltaR[direction.get(k)];
+					int col = c + Maze.deltaC[direction.get(k)];
+					if(checkBound(row, col, maze)) {
+						if(!visited[row][col]) {
+							dir = direction.get(k);
+							kill = true;
+							k = 4;
+						}
 					}
+				}
+				if(kill) {
+					carve(maze.map[r][c], dir);
+					r += Maze.deltaR[dir];
+					c += Maze.deltaC[dir];
 				}
 			} while(kill);
 
 			hunt = false;
 			for(int i = 0; i < maze.sizeR; ++i) {
 				for(int j = 0; j < maze.sizeC; ++j) {
-					if(visited[i][j] == false) {
+					if(!visited[i][j]) {
 						hunt = true;
-						for(int k = 0; k != 4; ++k) {
-							if(visited[i + Maze.deltaR[direction[k]]][j + Maze.deltaC[direction[k]]]) {
-								r = i;
-								c = j;
-								carve(maze.map[i][j], direction[k]);
-								i = maze.sizeR;
-								j = maze.sizeC;
+						for(int k = 0; k < 4; ++k) {
+							int row = i + Maze.deltaR[direction.get(k)];
+							int col = j + Maze.deltaC[direction.get(k)];
+							if(checkBound(row, col, maze)) {
+								if(visited[row][col]) {
+									r = i;
+									c = j;
+									carve(maze.map[r][c], direction.get(k));
+									i = maze.sizeR;
+									j = maze.sizeC;
+								}
 							}
 						}
 					}
@@ -64,9 +74,21 @@ public class HuntAndKillGenerator implements MazeGenerator {
 		}
 	}
 
+	private boolean checkBound(int r, int c, Maze maze) {
+		return r >= 0 && r < maze.sizeR && c >= 0 && c < maze.sizeC;
+	}
+
 	private void carve(Cell cell, int direction) {
-		cell.wall[direction].present = false;
-		cell.neigh[Maze.oppoDir[direction]].wall.present = false;
+		if(cell.wall[direction] == null) {
+			System.out.println("cell.wall[direction] : " + cell.r + " : " + cell.c + " : " + direction);
+		} else if(cell.neigh[direction] == null) {
+			System.out.println("cell.neigh[direction] : " + cell.r + " : " + cell.c + " : " + direction);
+		} else if(cell.neigh[direction].wall[Maze.oppoDir[direction]] == null) {
+			System.out.println("cell.neigh[direction].wall[Maze.oppoDir[direction]] : " + cell.r + " : " + cell.c + " : " + direction);
+		}else{
+			cell.wall[direction].present = false;
+			cell.neigh[direction].wall[Maze.oppoDir[direction]].present = false;
+		}
 	}
 
 	private void tunnelMazeGenerator(Maze maze) {
